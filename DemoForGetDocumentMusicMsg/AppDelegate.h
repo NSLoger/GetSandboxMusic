c6ed82d1a106c29fb77cc29aa6a204aee77e9a6d
王刚 <wanggang@thunder.com.cn>
2018-03-21 13:28:29 +0800

    Initial Commit

---
+++ DemoForGetDocumentMusicMsg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoForGetDocumentMusicMsg
+//
+//  Created by 王刚 on 2018/3/21.
+//  Copyright © 2018年 LOVER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
